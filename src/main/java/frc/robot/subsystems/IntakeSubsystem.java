61d94e399db5354927201a7c790639108cdfb0de
Miz <minirothmao@gmail.com>
2026-03-09 11:33:58 -0400

    Refactor IntakeSubsystem to CANFuelSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,59 +2,85 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+package frc.robot.subsystems;
 
-package frc.robot.commands;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
 
-
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.CANFuelSubsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import static frc.robot.Constants.FuelConstants.*;
 
+public class CANFuelSubsystem extends SubsystemBase {
+  private final SparkMax LeftIntakeLauncher;
+  private final SparkMax RightIntakeLauncher;
+  private final SparkMax Indexer;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class Intake extends Command {
-  /** Creates a new Intake. */
+  /** Creates a new CANBallSubsystem. */
+  public CANFuelSubsystem() {
+    // create brushed motors for each of the motors on the launcher mechanism
+    LeftIntakeLauncher = new SparkMax(LEFT_INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushless);
+    RightIntakeLauncher = new SparkMax(RIGHT_INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushless);
+    Indexer = new SparkMax(INDEXER_MOTOR_ID, MotorType.kBrushed);
 
+    // create the configuration for the feeder roller, set a current limit and apply
+    // the config to the controller
+    SparkMaxConfig feederConfig = new SparkMaxConfig();
+    feederConfig.smartCurrentLimit(INDEXER_MOTOR_CURRENT_LIMIT);
+    Indexer.configure(feederConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-  CANFuelSubsystem fuelSubsystem;
+    // create the configuration for the launcher roller, set a current limit, set
+    // the motor to inverted so that positive values are used for both intaking and
+    // launching, and apply the config to the controller
+    SparkMaxConfig launcherConfig = new SparkMaxConfig();
 
+    launcherConfig.smartCurrentLimit(LAUNCHER_MOTOR_CURRENT_LIMIT);
+    launcherConfig.voltageCompensation(12);
+    launcherConfig.idleMode(IdleMode.kCoast);
+    RightIntakeLauncher.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    launcherConfig.inverted(true);
+    LeftIntakeLauncher.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-  public Intake(CANFuelSubsystem fuelSystem) {
-    addRequirements(fuelSystem);
-    this.fuelSubsystem = fuelSystem;
+    // put default values for various fuel operations onto the dashboard
+    // all commands using this subsystem pull values from the dashbaord to allow
+    // you to tune the values easily, and then replace the values in Constants.java
+    // with your new values. For more information, see the Software Guide.
+    SmartDashboard.putNumber("Intaking feeder roller value", INDEXER_INTAKING_PERCENT);
+    SmartDashboard.putNumber("Intaking intake roller value", INTAKE_INTAKING_PERCENT);
+    SmartDashboard.putNumber("Launching feeder roller value", INDEXER_LAUNCHING_PERCENT);
+    SmartDashboard.putNumber("Launching launcher roller value", LAUNCHING_LAUNCHER_PERCENT);
+    //SmartDashboard.putNumber("Spin-up feeder roller value", SPIN_UP_FEEDER_VOLTAGE);
   }
 
-
-  // Called when the command is initially scheduled. Set the rollers to the
-  // appropriate values for intaking
-  @Override
-  public void initialize() {
-    fuelSubsystem
-        .setIntakeLauncherRoller(SmartDashboard.getNumber("Intaking intake roller value", INTAKE_INTAKING_PERCENT));
-    fuelSubsystem.setFeederRoller(SmartDashboard.getNumber("Intaking feeder roller value", INDEXER_INTAKING_PERCENT));
+  // A method to set the voltage of the intake roller
+  public void setIntakeLauncherRoller(double power) {
+    LeftIntakeLauncher.set(power);
+    RightIntakeLauncher.set(power); // positive for shooting
   }
 
-
-  // Called every time the scheduler runs while the command is scheduled. This
-  // command doesn't require updating any values while running
-  @Override
-  public void execute() {
+  // A method to set the voltage of the intake roller
+  public void setFeederRoller(double power) {
+    Indexer.set(power); // positive for shooting
   }
 
-
-  // Called once the command ends or is interrupted. Stop the rollers
-  @Override
-  public void end(boolean interrupted) {
-    fuelSubsystem.setIntakeLauncherRoller(0);
-    fuelSubsystem.setFeederRoller(0);
+  // A method to stop the rollers
+  public void stop() {
+    Indexer.set(0);
+    LeftIntakeLauncher.set(0);
+    RightIntakeLauncher.set(0);
   }
 
-
-  // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
-    return false;
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
-
